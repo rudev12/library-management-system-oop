7fd771951b0aed632dd0e9be27b568b46874da62
Rudhraksh shukla <rudhrakshshukla@gmail.com>
2026-06-07 21:03:31 +0530

    Add book_id to Book class and update methods

--- Library Management System.py
+++ Library Management System.py
@@ -1,29 +1,31 @@
 class Book:
-    def __init__(self,title,author):
+    def __init__(self,book_id,title,author):
+        self.book_id = book_id
         self.title = title
         self.author = author
         self.available = True
     def show_book(self):
         print("=====Book-Info=====")
+        print("Book Id:",self.book_id)
         print("Title:",self.title)
         print("Author:",self.author)
         print("Available:",self.available)
         
     def borrow_book(self):
-        if self.available == True:
+        if self.available :
             print("Book Borrowed Successfully")
             self.available = False
         else:
             print("Book Already Borrowed")
             
     def return_book(self):
-        if self.available == False:
+        if not self.available:
             print("Book Returned Successfully")
             self.available = True
         else:
             print("Book Was Not Borrowed")
         
-b1 = Book("Python Basics", "Rudhraksh")
+b1 = Book(101,"Python Basics", "Rudhraksh")
 
 b1.show_book()
 
