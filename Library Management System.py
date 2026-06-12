29ac9aabdce21b093f5f312d7160bac8576c1153
Rudhraksh shukla <rudhrakshshukla@gmail.com>
2026-06-12 21:20:43 +0530

    Implement Library and Book classes with basic methods

--- Library Management System.py
+++ Library Management System.py
@@ -1,3 +1,39 @@
+class Library:
+    def __init__(self, library_name, address):
+        self.library_name = library_name
+        self.address = address
+        self.books = {}
+        self.number_employees = 0
+
+    def show_library(self):
+        print("Name of library:",self.library_name)
+        print("Address:",self.address)
+        print("Total Books:",len(self.books))
+        print("Total Employee Working:", self.number_employees)
+        
+    def add_book(self,book):
+        self.books[book.book_id] = book
+        print("Book Added Successfully")
+        
+    def search_book(self,book_id):
+        if book_id == "   ":
+            print("Book Id Cannot be Empty")
+        elif book_id not in self.books:
+            print("Book Id Not Exists Please Enter Valid Id")
+        else:
+            return self.books[book_id]
+            
+    def remove_book(self,book_id):
+        if book_id == "   ":
+            print("Book Id Cannot be Empty")
+        elif book_id not in self.books:
+            print("Book Id Not Exists Please Enter Valid Id")
+        else:
+            book =self.books[book_id]
+            del self.books[book_id]
+            print("Book Removed Sucessfully")
+            return book
+
 class Book:
     def __init__(self,book_id,title,author):
         self.book_id = book_id
@@ -34,9 +70,10 @@ class Book:
             print("Title:",self.title)
         
 b1 = Book(101,"Python Basics", "Rudhraksh")
-
-b1.show_book()
-
-b1.update_title("Advanced Python")
-
-b1.show_book()
+b2 = Book(102,"C++ Basics ","Swayam")
+l1 = Library("Mumbai Library", "Mumbai")
+l1.add_book(b1)
+l1.add_book(b2)
+book = l1.remove_book(101)
+book.show_book()
+b2.show_book()
