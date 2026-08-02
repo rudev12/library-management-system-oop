ff52f603d4340173ac450390ad3bdeec50aa1faf
Rudhraksh shukla <rudhrakshshukla@gmail.com>
2026-08-02 15:48:08 +0530

    Refactor Library class to use database for book management

--- library.py
+++ library.py
@@ -1,8 +1,11 @@
+import book as Book
+import database as db
 class Library:
-    def __init__(self, library_name, address):
+    def __init__(self, library_name, address,db):
         self.library_name = library_name
         self.address = address
         self.books = {}
+        self.db =db
         self.number_employees = 0
 
     def show_library(self):
@@ -11,12 +14,14 @@ class Library:
         print("Total Books:",len(self.books))
         print("Total Employee Working:", self.number_employees)
         
-    def add_book(self,book):
-        if book.book_id in self.books:
-            print("Book Alredy Exists, Please Enter Valid Book Id")
+    def add_book(self, book):
+        if self.db.book_checking(book.title,book.author):
+            print("Book Already Exisit")
         else:
-            self.books[book.book_id] = book
-            self.save_books()
+            self.db.insert_book(
+            book.title,
+            book.author,
+            book.genre)
             print("Book Added Successfully")
         
     def search_book(self,book_id):
@@ -26,20 +31,35 @@ class Library:
             return self.books[book_id]
             
     def remove_book(self,book_id):
-        if book_id not in self.books:
-            print("Book Id Not Exists Please Enter Valid Id")
-        else:
-            book = self.books[book_id]
-            self.save_deleted_book(book)
-            del self.books[book_id]
-            print("Book Removed Successfully")
-            self.save_books()
-
+       if not self.db.book_id_checking(book_id):
+           print("Book Not Found")
+           return
+       else:
+           while True:
+            confirmation = input(
+            "Are You Sure You Want To Delete This Book? (yes/no): "
+            )
+            if confirmation.lower() =="yes":
+                succes = self.db.move_to_deleted_books(book_id)
+                if succes:
+                      print("Book Deleted Succesfully")
+                      break
+                else:
+                    print("Book Deletion Failed")
+                    
+            elif confirmation.lower() =="no":
+                print("Action cancelled!")
+                break
+            else:
+                print("Invalid Input Plz,Try Again!")
+     
     def show_all_books(self):
-        if len(self.books) == 0:
-            print("No Books Available In Library")
+        books =self.db.fetch_books()
+
+        if len(books)==0:
+            print("No Books Available in Library")
         else:
-            for book in self.books.values():
+            for book in books:
                 book.show_book()
                 
     def library_stats(self):
@@ -58,291 +78,69 @@ class Library:
             print("Total Available Books:",available_books)
             print("Total Borrowed Books:",borrowed_books)
             
-    def search_by_title(self,book_title):
-        matched_book = []
-        
-        if book_title.strip()== "":
-            print("Title Cannot Be Empty")
+    def search_by_title(self,title):
+        books = self.db.check_by_title(title)
+        if not books:
+            print("Book Not Found")
         else:
-            for book in self.books.values():
-                if book_title.lower() in book.title.lower():
-                    matched_book.append(book)
-
-        if len(matched_book) == 0:
-            print("No Book Found")
-        else:
-            print("Total Books Found:",len(matched_book))
-            for book in matched_book:
-                book.show_book()
-        return matched_book
-        
+            for book in books:
+               book.show_book()
         
     def search_by_author(self,author):
-        matched_author = []
-        if author.strip() == "":
-            print("Author Name Cannot Be Empty")
-            return []
-            
-        for book in self.books.values(): 
-            if author.lower() in book.author.lower():
-                matched_author.append(book)
-                
-        
-        if len(matched_author) == 0:
-            print("No Author's Book Found")
-        else:
-            print("Total Books By Author :",(len(matched_author)))
-            for book in matched_author:
-                book.show_book()
-        
-        return matched_author
-    
+       books = self.db.check_by_author(author)
+       if not books:
+          print("Book Not Found")
+       else:
+          for book in books:
+            book.show_book()
+
     def check_availability(self,book_id):
-        book = self.search_book(book_id)
-        if book is not None:
-            if book.available:
-                print(book_id,":","Book is Available")
-            else:
-                print(book_id,":","Book Not Available")
-  
-    def load_book(self):
-        try:
-            with open("library_data.txt","r") as file:
-                for line in file:
-                    data = line.strip().split(",")
-                    if len(data) == 4:
-                        book_id, title, author, available = data
-                        book = Book(int(book_id),title,author,available = available == "True")
-                        self.books[int(book_id)] = book
-                    else:
-                        print("Invalid data format in line:", line)
-                        continue
-        except FileNotFoundError:
-            print("No Book Data Found")
+        if not self.db.book_id_checking(book_id):
+            print("Book Not Found")
+            return
+        
+        if self.db.book_availability_checking(book_id):
+            print("Book Is Available")
+        else:
+            print("Book Id Borrowed")
 
     def borrow_book(self,book_id):
-        if book_id not in self.books:
-            print("Book Not Exist")
-        else:
-            book = self.search_book(book_id)
-            if book.available:
-                book.available = False
-                self.save_books()
-                print("Book Borrowed Successfully")
-            else:
-                print("Book already borrowed.")
+        if not self.db.book_id_checking(book_id):
+            print("Book Not Found")
+            return
+        
+        if not self.db.book_availability_checking(book_id):
+            print("Book Already Borrowed")
+            return
+
+        self.db.update_availability(book_id,"Borrowed")
+        print("Book Borrowed Successfully")
 
     def return_book(self,book_id):
-        if book_id not in self.books:
-            print("Book Not Exist")
-        else:
-            book = self.search_book(book_id)
-            if not book.available :
-                book.available = True
-                self.save_books()
-                print("Book Returned Successfully")
-            else:
-                print("Book already Returned .")
-
-    def save_books(self):
-        with open("library_data.txt","w") as file:
-                    for b in self.books.values():
-                        file.write(f"{b.book_id},{b.title},{b.author},{b.available}\n")
-                    print("Book Data Saved Successfully")
-    
-    def update_title(self,book_id,new_title):
-        if book_id not in self.books:
-            print("Book Not Exist")
-        else:
-            book = self.search_book(book_id)
-            book.update_title(new_title)
-            self.save_books()
-
-    def generate_book_id(self):
-        if len(self.books) == 0:
-            return 101
-        
-        return max(self.books.keys()) +1 
-
-    def save_deleted_book(self,book):
-        with open("deleted_books.txt","a") as file:
-            file.write(f"{book.book_id},{book.title},{book.author},{book.available}\n") 
-    
-    def load_deleted_books(self):
-        deleted_books = []
-        try:
-            with open("deleted_books.txt","r") as file:
-                for line in file:
-                    data = line.strip().split(",")
-                    if len(data) == 4:
-                        book_id ,title,author,available = data
-                        book = Book(
-                            int(book_id),title,author,available = available =="True"
-                            )
-                        deleted_books.append(book)
-        except FileNotFoundError:
-            print("No Deleted Book Found")
-        
-        return deleted_books
-    
-    def show_deleted_books(self):
-        deleted_books = self.load_deleted_books()
-        if not deleted_books:
-            print("No Deleted Book Found")
-        else:
-            print("Total Deleted Books:",len(deleted_books))
-            for book in deleted_books:
-                book.show_book()
+        if not self.db.book_id_checking(book_id):
+            print("Book Not Found")
+            return
+        if self.db.book_availability_checking(book_id):
+            print("Book Already Available")
+            return
+        self.db.update_availability(book_id,"Available")
+        print("Book Returned Successfully")
 
     def restore_deleted_book(self, book_id):
-        deleted_books = self.load_deleted_books()
-
-        if not deleted_books:
-            print("No Book Found In Deleted Books")
+        if self.db.book_id_checking(book_id):
+            print("Book Not Found")
             return
-        found = False
-
-        for book in deleted_books:
-
-            if book.book_id == book_id:
-                found = True
-                if book.book_id in self.books:
-                    print("Book Already In Library")
-                    return
-
-                print("\nBook Found")
-                book.show_book()
-
-                confirmation = input(
-                    "Are you sure you want to restore this book? (yes/no): "
-                    )
-
-                if confirmation.lower() == "yes":
-                    self.books[book.book_id] = book
-                    self.save_books()
-                    deleted_books.remove(book)
-                    with open("deleted_books.txt", "w") as file:
-                        for b in deleted_books:
-                            file.write(
-                                f"{b.book_id},{b.title},{b.author},{b.available}\n"
-                            )
-
-                    print("Book Restored Successfully")
-
-                elif confirmation.lower() == "no":
-                    print("Restoration Cancelled")
-
-                else:
-                    print("Invalid Input")
-
-                return
-
-        if not found:
-            print("Book Id Not Found")
-
-    def restore_all_deleted_books(self):
-        deleted_books = self.load_deleted_books()
-        if not deleted_books:
-            print("No Deleted Book Found")
         else:
-            for book in deleted_books:
-                if book.book_id not in self.books:
-                    self.books[book.book_id] = book
-            self.save_books()
-class Book:
-    def __init__(self,book_id,title,author,available = True):
-        self.book_id = book_id
-        self.title = title
-        self.author = author
-        self.available = available
-    def show_book(self):
-        print("=====Book-Info=====")
-        print("Book Id:",self.book_id)
-        print("Title:",self.title)
-        print("Author:",self.author)
-        print("Available:",self.available)
-        
-l1 = Library("Mumbai Library", "Mumbai")
-l1.load_book()
-l1.load_deleted_books()
-while True:
-    print("\n===== LIBRARY MENU =====")
-    print("1. Add Book")
-    print("2. Remove Book")
-    print("3. Search By Title")
-    print("4. Search By Author")
-    print("5. Borrow Book")
-    print("6. Return Book")
-    print("7. Show All Books")
-    print("8. Library Statistics")
-    print("9. Restore Deleted Books")
-    print("10. Exit")
-    
-    choice = input("Enter your Choice:" )
-    
-    if choice == "1":
-        book_id = l1.generate_book_id()
-        print("Genrated Book Id:",book_id)
-        title = input("Enter a Book Title: ")
-        author = input("Enter Author Name: ")
-        book = Book(book_id,title,author)
-        l1.add_book(book)
-        
-    elif choice == "2":
-        try:
-            book_id = int(input("Enter Book Id: "))
-        except ValueError:
-            print("Book Id Cannot be Alphabet, Plz Try Again")
-            continue
-        
-        l1.remove_book(book_id)
-        
-    elif choice == "3":
-        book_title = input("Enter Book Title: ")
-        
-        l1.search_by_title(book_title)
-    
-    elif choice == "4":
-        author = input("Enter Author Name: ")
-      
-        l1.search_by_author(author)
-        
-    elif choice == "5":
-        try:
-            book_id = int(input("Enter Book Id: "))
-        except ValueError:
-            print("Book Id Cannot be Alphabet, Plz Try Again")
-            continue
-        
-        l1.borrow_book(book_id)
-        
-    elif choice == "6":
-        try:
-            book_id = int(input("Enter Book Id: "))
-        except ValueError:
-            print("Book Id Cannot be Alphabet, Plz Try Again")
-            continue
-        
-        l1.return_book(book_id)
-        
-    elif choice == "7":
-        
-        l1.show_all_books()
-        
-    elif choice == "8":
-        
-        l1.library_stats()
-    elif choice == "9":
-        try:
-            book_id =int(input("Enter Book id :"))
-            l1.restore_deleted_book(book_id)
-            
-        except ValueError:
-            print("Book id cannot Be Alphabet")
-        
-        
-    elif choice == "10":
-        print("Exiting")
-        break
-    else:
-        print("Invalid Choice")
+            while True:
+               confirmation = input(
+                   "Are You Sure You Want To Restor This Book(yes/no)"
+                    )
+               if confirmation.lower() =="yes":
+                    self.db.restore_deleted_books(book_id)
+                    print("Book Restore Succesfully")
+                    break
+               elif confirmation.lower() == "no":
+                    print("Action cancelled!")
+                    break
+               else:
+                    print("Invalid Input,Try Again!")
